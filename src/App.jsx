09e76214b1c151a5635974906655a169b563aa63
Gaurav01212 <130878210+Gaurav01212@users.noreply.github.com>
2024-01-25 22:42:05 +0530

    added a note Div in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
 import Pokedex from "./components/Pokedex/Pokedex"
 import CustomRoutes from './routes/CustomRoutes.jsx'
+import './App.css'
 
 
 function App() {
+
+const[isDisplay,setIsDisplay]=useState(true);
+
+const changeDisplayProp=()=>{
+  setIsDisplay(false);
+}
+
+setTimeout(changeDisplayProp,7000);
+
+
+
  
 
     
@@ -10,7 +23,12 @@ function App() {
 
  
   return (
+    <>
+    {(isDisplay)?<div className="alert">Note : <span>Venusaur</span> , 
+     <span>Ivysuar</span>  Is Not Avaialble In The Database.Sorry for your inconveniance Caused...! </div>:""}
     <CustomRoutes/>
+    </>
+    
   )
 }
 
